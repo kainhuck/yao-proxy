8eeb368d442c2f8f6e7b7786b1ac90ec4d1b10cf
xiaor <xiaorang.hu@tuya.com>
2021-08-27 16:07:52 +0800

    add config

---
+++ internal/service/local/config.go
@@ -0,0 +1,47 @@
+package local
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Config struct {
+	Port       int    `json:"port"`
+	Key        string `json:"key"`
+	RemoteHost string `json:"remote_host"`
+	RemotePort int    `json:"remote_port"`
+}
+
+func ReadConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	bts, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg = Config{}
+	err = json.Unmarshal(bts, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.RemoteHost == "" {
+		return nil, fmt.Errorf("need remote host")
+	}
+	if cfg.RemotePort == 0 {
+		cfg.RemotePort = 20807
+	}
+	if cfg.Port == 0 {
+		cfg.Port = 20808
+	}
+	if cfg.Key == "" {
+		cfg.Key = "Atu@&^_^&Ak1314$$"
+	}
+	return &cfg, nil
+}
