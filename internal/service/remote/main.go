e60d7e656dd8b290fd07c635491baac392e4b1c9
xiaor <xiaorang.hu@tuya.com>
2021-09-01 14:34:13 +0800

    fix IPv6 bug

--- internal/service/remote/main.go
+++ internal/service/remote/main.go
@@ -1,15 +1,11 @@
 package remote
 
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
 	YPCipher "github.com/kainhuck/yao-proxy/internal/cipher"
-	YPConn "github.com/kainhuck/yao-proxy/internal/conn"
 	"github.com/kainhuck/yao-proxy/internal/log"
-	"net"
 	"os"
-	"strconv"
 	"sync"
 )
 
@@ -40,32 +36,4 @@ func Main() {
 	}
 
 	wg.Wait()
-}
-
-// 获取目标地址 string 类型
-func getTargetAddr(conn *YPConn.Conn) (string, error) {
-	buff := make([]byte, 256)
-
-	_, err := conn.Read(buff)
-	if err != nil {
-		return "", err
-	}
-
-	length := buff[0] // ip + 端口(2) 的长度
-
-	// 判断类型
-	var host string
-	switch buff[1] {
-	case 1: // IPv4
-		host = net.IP(buff[2:length]).String()
-	case 3: // Domain
-		host = string(buff[2:length])
-	case 4: // IPv6
-		host = net.IP(buff[2:length]).String()
-	}
-
-	port := binary.BigEndian.Uint16(buff[length : length+2])
-	host = net.JoinHostPort(host, strconv.Itoa(int(port)))
-
-	return host, nil
 }
