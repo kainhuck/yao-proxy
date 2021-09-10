5d7a740f3aa836f443fee54f17dd7f9101ebbe4a
kainhuck <1628321264@qq.com>
2021-09-10 16:36:47 +0800

    go fmt

--- internal/service/local/server.go
+++ internal/service/local/server.go
@@ -116,38 +116,38 @@ func (s *Server) handleConn(conn net.Conn) {
 
 	// 3. 给浏览器发送成功响应
 	/*
-		 The SOCKS request information is sent by the client as soon as it has
-		   established a connection to the SOCKS server, and completed the
-		   authentication negotiations.  The server evaluates the request, and
-		   returns a reply formed as follows:
+	 The SOCKS request information is sent by the client as soon as it has
+	   established a connection to the SOCKS server, and completed the
+	   authentication negotiations.  The server evaluates the request, and
+	   returns a reply formed as follows:
 
-		        +----+-----+-------+------+----------+----------+
-		        |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
-		        +----+-----+-------+------+----------+----------+
-		        | 1  |  1  | X'00' |  1   | Variable |    2     |
-		        +----+-----+-------+------+----------+----------+
+	        +----+-----+-------+------+----------+----------+
+	        |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	        +----+-----+-------+------+----------+----------+
+	        | 1  |  1  | X'00' |  1   | Variable |    2     |
+	        +----+-----+-------+------+----------+----------+
 
-		     Where:
+	     Where:
 
-		          o  VER    protocol version: X'05'
-		          o  REP    Reply field:
-		             o  X'00' succeeded
-		             o  X'01' general SOCKS server failure
-		             o  X'02' connection not allowed by ruleset
-		             o  X'03' Network unreachable
-		             o  X'04' Host unreachable
-		             o  X'05' Connection refused
-		             o  X'06' TTL expired
-		             o  X'07' Command not supported
-		             o  X'08' Address type not supported
-		             o  X'09' to X'FF' unassigned
-		          o  RSV    RESERVED
-		          o  ATYP   address type of following address
-		             o  IP V4 address: X'01'
-		             o  DOMAINNAME: X'03'
-		             o  IP V6 address: X'04'
-		          o  BND.ADDR       server bound address
-		          o  BND.PORT       server bound port in network octet order
+	          o  VER    protocol version: X'05'
+	          o  REP    Reply field:
+	             o  X'00' succeeded
+	             o  X'01' general SOCKS server failure
+	             o  X'02' connection not allowed by ruleset
+	             o  X'03' Network unreachable
+	             o  X'04' Host unreachable
+	             o  X'05' Connection refused
+	             o  X'06' TTL expired
+	             o  X'07' Command not supported
+	             o  X'08' Address type not supported
+	             o  X'09' to X'FF' unassigned
+	          o  RSV    RESERVED
+	          o  ATYP   address type of following address
+	             o  IP V4 address: X'01'
+	             o  DOMAINNAME: X'03'
+	             o  IP V6 address: X'04'
+	          o  BND.ADDR       server bound address
+	          o  BND.PORT       server bound port in network octet order
 	*/
 	if _, err := conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}); err != nil {
 		s.logger.Errorf("reply to browser error: %v", err)
@@ -302,7 +302,7 @@ func (s *Server) getCipherRemote() *CipherRemote {
 
 func (s *Server) getRemoteConn() *YPConn.Conn {
 	cr := s.getCipherRemote()
-	conn, err := YPConn.Dial(cr.RemoteAddr, cr.cipher.Copy(), 10 * time.Second)
+	conn, err := YPConn.Dial(cr.RemoteAddr, cr.cipher.Copy(), 10*time.Second)
 	if err != nil {
 		return s.getRemoteConn()
 	}
